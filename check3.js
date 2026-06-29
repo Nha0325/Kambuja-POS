d556a9a8379313831e18ac7f95cf4d0d69a7ca9e
Nha0325 <selenagamingti2@gmail.com>
2026-06-30 02:51:16 +0700

    Update tests

--- check3.js
+++
@@ -1,3 +0,0 @@
-const fs = require('fs');
-const km = JSON.parse(fs.readFileSync('Frontend/src/locales/km.json', 'utf8'));
-console.log('km.create_admin_owner:', km['create_admin_owner']);
--- test-search.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config({ path: 'Backend/.env' });
-const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI).then(async () => {
-    const Product = require('./Backend/models/inventory/Product.model');
-    const Sale = require('./Backend/models/sales/Sale.model');
-    const p = await Product.find({ $text: { $search: 'a' } }).limit(2);
-    console.log('Products:', p.length);
-    mongoose.connection.close();
-});
